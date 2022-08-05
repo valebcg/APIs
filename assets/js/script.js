876c6b4c57a072fcc4a4a2864b7183403472df41
valebcg <valeria.cortes.g@gmail.com>
2022-08-05 19:35:31 -0400

    add style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,44 @@
 let inputAmount= document.querySelector("#inputAmount");
 let selectTypeMoney= document.querySelector ("#selectTypeMoney");
 let btn= document.querySelector ("#btn");
-let dataArray = [] //Guarda el array que se obtiene del servido
+let dataArray = [] //Guarda el array que se obtiene del servidor
 let myChart = null
 let lastchange = 0;
 
 async function ConectionServer() {
   try {
-    let currency = selectTypeMoney.value;
+    currency = selectTypeMoney.value;
     const res = await fetch(`https://mindicador.cl/api/${currency}`)
     const data = await res.json()
-    let lastchange = (data.serie[0].valor);
+     lastchange = (data.serie[0].valor);
     let test = (data.serie);
     calculo();
-    let dataArray = data.serie // Aca es donde guardo el array original en la variable dataArray
-    const datChange = dataArray.map((x) => x.valor) //Aca creo una nueva variable para que tome las fechas Label
-    const datLabel = dataArray.map((x) => x.fecha)   //Aca creo una nueva variable para que tome las fechas Label
-  
+
+    dataArray = data.serie // Aca es donde guardo el array original en la variable dataArray
+    const datChange = dataArray.map((x) => x.valor)  //Aca creo una nueva variable para que tome las fechas Label
+    const datLabel = dataArray.map((x) => x.fecha) //Aca creo una nueva variable para que tome las fechas Label
+
     //Aca envio los datos al grafico
     chartRender(datLabel, datChange)
     document.querySelector("#error").innerHTML = 'Mensaje del Servidor: conexión realizada'
   } catch (e) {
      document.querySelector("#error").innerHTML = 'Mensaje del Servidor: No se pudo establecer la conexión'
-     document.querySelector("#resultado").innerHTML= "....";
-    
+     document.querySelector("#resultado").innerHTML= "...";
+   
   }
 }
 
 
 //*Este es el grafico**
 function chartRender(datLabel, datChange) {
+  
   const ctx = document.getElementById('myChart').getContext('2d')
   if (myChart != null) {
     myChart.destroy()
   }
   myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'bar',
+
     data: {
       labels: datLabel.reverse(),
       datasets: [
@@ -65,7 +68,7 @@ function chartRender(datLabel, datChange) {
 
 function calculo () {
   let resultado = Number((inputAmount.value / lastchange).toFixed(2));
-  document.querySelector("#resultado").innerHTML= "Resultado  :  " + resultado;
+ document.querySelector("#resultado").innerHTML= "Resultado  :  " + resultado;
 }
 
 btn.addEventListener("click", () =>{
@@ -78,5 +81,7 @@ btn.addEventListener("click", () =>{
     alert('Solo puedes ingresar valores numéricos en la cantidad aconvertir')
     return
   }
-ConectionServer()
+
+
+  ConectionServer()
 })
